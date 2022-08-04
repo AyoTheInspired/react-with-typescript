c37ac3d025fe99abc8509058dddcf674cc5ba2d0
AyoTheInspired <ayoinspired@gmail.com>
2022-08-04 09:52:00 +0100

    Completed app

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -14,25 +14,76 @@ export default function SingleTodo({
 	todos,
 	setTodos,
 }: ISingleTodoProps) {
+	const [edit, setEdit] = React.useState<boolean>(false);
+	const [editTodo, setEditTodo] = React.useState<string>(todo.todo);
+
+	const handleDone = (id: number) => {
+		setTodos(
+			todos.map((todo) =>
+				todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+			)
+		);
+	};
+
+	const handleEdit = (e: React.FormEvent, id: number) => {
+		e.preventDefault();
+
+		setTodos(
+			todos.map((todo) =>
+				todo.id === id
+					? {
+							...todo,
+							todo: editTodo,
+					  }
+					: todo
+			)
+		);
+
+		setEdit(false);
+	};
+
 	return (
 		<form
+			onSubmit={(e) => handleEdit(e, todo.id)}
 			style={{
 				display: "flex",
 				margin: "20px auto",
 				border: "1px solid green",
 				padding: "20px",
 			}}>
-			<span className="todos__single--text" style={{ marginRight: "25px" }}>
-				{todo.todo}
-			</span>
+			{edit ? (
+				<input
+					value={editTodo}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+						setEditTodo(e.target.value)
+					}
+				/>
+			) : todo.isDone ? (
+				<s
+					className="todos__single--text"
+					style={{ marginRight: "25px", color: "red" }}>
+					{todo.todo}
+				</s>
+			) : (
+				<span className="todos__single--text" style={{ marginRight: "25px" }}>
+					{todo.todo}
+				</span>
+			)}
+
 			<div>
 				<span className="icon">
-					<AiFillEdit />
+					<AiFillEdit
+						onClick={() => {
+							if (!edit && !todo.isDone) {
+								setEdit(!edit);
+							}
+						}}
+					/>
 				</span>
 				<span className="icon">
 					<AiFillDelete />
 				</span>
-				<span className="icon">
+				<span className="icon" onClick={() => handleDone(todo.id)}>
 					<MdDone />
 				</span>
 			</div>
